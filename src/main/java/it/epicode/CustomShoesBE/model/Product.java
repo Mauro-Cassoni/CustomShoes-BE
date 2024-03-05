a88f2ed395b54a996b0f6f770342f603d6f2321f
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2024-03-05 16:41:24 +0100

    product

---
+++ src/main/java/it/epicode/CustomShoesBE/model/Product.java
@@ -0,0 +1,31 @@
+package it.epicode.CustomShoesBE.model;
+
+import it.epicode.CustomShoesBE.enums.Category;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String img;
+
+    private String name;
+
+    private String brand;
+
+    private Category category;
+
+    private String description;
+
+    private double size;
+
+    private String color;
+
+    private double price;
+
+}
